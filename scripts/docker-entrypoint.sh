430f0ec66902c26627ab11cde447466181d7ed2c
Rostyslav Nakarlovich <zvesdochyot@gmail.com>
2024-05-04 22:39:54 +0300

    Added changing folder owner to runner-user

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -11,14 +11,12 @@ if [[ ! -r /var/run/docker.sock || ! -w /var/run/docker.sock ]]; then
 fi
 
 GROUP_ID=$(stat -c '%g' '/var/run/docker.sock')
-echo ${GROUP_ID}
 
 if ! getent group "$GROUP_ID" > /dev/null; then
 	addgroup --gid "$GROUP_ID" docker
 fi
 
 GROUP_NAME=$(getent group "$GROUP_ID" | cut -d: -f1)
-echo ${GROUP_NAME}
 
 if ! groups runner-user | grep --quiet "\b${GROUP_NAME}\b"; then
 	usermod --append --groups "$GROUP_ID" runner-user
@@ -29,6 +27,7 @@ TOKEN=$ACCESS_TOKEN
 API_VERSION=$GITHUB_API_VERSION
 
 REGISTRATION_TOKEN=$(curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: ${API_VERSION}" https://api.github.com/orgs/${ORG_NAME}/actions/runners/registration-token | jq .token --raw-output)
+mkdir -p ./_work && chown -R runner-user ./_work
 sudo -u runner-user ./config.sh --unattended --url https://github.com/${ORG_NAME} --token ${REGISTRATION_TOKEN} --labels docker-runner
 
 cleanup() {
