b0147d1ea8eaf9ca1e4f2d83ccbb7fa6e7f45892
Rostyslav Nakarlovich <zvesdochyot@gmail.com>
2024-10-16 18:41:08 +0300

    Fixed headers feeding into the script

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -42,13 +42,13 @@ ACCEPT_HEADER="Accept: application/vnd.github+json"
 AUTHORIZATION_HEADER="Authorization: Bearer ${ACCESS_TOKEN}"
 API_VERSION_HEADER="X-GitHub-Api-Version: ${API_VERSION}"
 
-REGISTRATION_TOKEN=$(curl -L -X POST -H ${ACCEPT_HEADER} -H ${AUTHORIZATION_HEADER} -H ${API_VERSION_HEADER} ${REGISTRATION_URL} | jq .token --raw-output)
+REGISTRATION_TOKEN=$(curl -L -X POST -H "${ACCEPT_HEADER}" -H "${AUTHORIZATION_HEADER}" -H "${API_VERSION_HEADER}" ${REGISTRATION_URL} | jq .token --raw-output)
 sudo -u runner-user ./config.sh --unattended --url ${ENTITY_URL} --token ${REGISTRATION_TOKEN} --work ${WORKING_DIRECTORY} --labels docker-runner
 
 cleanup() {
     echo "Started removing runner from ${ENTITY_URL}."
 
-    REMOVE_TOKEN=$(curl -L -X POST -H ${ACCEPT_HEADER} -H ${AUTHORIZATION_HEADER} -H ${API_VERSION_HEADER} ${REMOVE_URL} | jq .token --raw-output)
+    REMOVE_TOKEN=$(curl -L -X POST -H "${ACCEPT_HEADER}" -H "${AUTHORIZATION_HEADER}" -H "${API_VERSION_HEADER}" ${REMOVE_URL} | jq .token --raw-output)
     sudo -u runner-user ./config.sh remove --token ${REMOVE_TOKEN}
 
     echo "Successfully finished removing runner from ${ENTITY_URL}."
